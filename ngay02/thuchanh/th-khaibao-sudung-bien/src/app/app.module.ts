d423c28ebd22f4057a83ace3852ba375bec214b9
unknown <thjsang6596@gmail.com>
2021-05-05 08:32:00 +0700

    test angular

---
+++ ngay02/thuchanh/th-khaibao-sudung-bien/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppComponent} from './app.component';
+import {WordComponent} from './word.component';
+import {from} from 'rxjs';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WordComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
---
+++ ngay02/thuchanh/th-khaibao-sudung-bien/src/app/word.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  template: '<h3>Test Angular</h3>',
+  selector: 'app-word'
+})
+
+export class WordComponent {}
