f18af14ff07c4b13652a1afd12ec38a8c0b7b4cc
unknown <thjsang6596@gmail.com>
2021-05-11 11:39:52 +0700

    excersives-review done !

--- excersive-review/product/src/app/product-homepage/list-product/create-product/create-product.component.ts
+++ excersive-review/product/src/app/product-homepage/list-product/create-product/create-product.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Product} from "../../../model/Product";
 
 @Component({
   selector: 'app-create-product',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-product.component.scss']
 })
 export class CreateProductComponent implements OnInit {
-
-  constructor() { }
+  @Output('productNew') newProduct = new EventEmitter<Product>()
+  public product:Product;
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  createProduct(id: string, name: string, price: string, date: string, quantity: string, origin: string) {
+    this.product=new Product(parseInt(id),name,parseInt(price),new Date(date),parseInt(quantity),origin)
+    this.newProduct.emit(this.product);
+  }
 }
--- excersive-review/product/src/app/product-homepage/list-product/list-product.component.ts
+++ excersive-review/product/src/app/product-homepage/list-product/list-product.component.ts
@@ -10,6 +10,7 @@ export class ListProductComponent implements OnInit {
 
   public origin:string[]=['VN','CN','USA','ENG'];
   public productDetail:Product;
+  public product:Product;
 
   public productList:Product[]=[
     new Product(1,"dien thoai di dong", 1000, new Date('2021-06-05'), 5, this.origin[0]),
@@ -18,8 +19,17 @@ export class ListProductComponent implements OnInit {
     new Product(4,"ti vi ban", 1000, new Date('2021-06-10'), 20, this.origin[3])
   ]
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  showDetailProduct(pro: Product) {
+    this.productDetail=pro;
+  }
+
+  addNew(item: Product) {
+    this.productList.push(item)
   }
 }
